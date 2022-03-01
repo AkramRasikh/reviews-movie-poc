b501f596826ff5d8d3a465de361554e0311dd5af
Akram Rasikh <akram.rasikh@gmail.com>
2022-03-01 12:48:20 +0000

    like to dislike reimplemented

--- __test__/integration/like-to-dislike.test.js
+++ __test__/integration/like-to-dislike.test.js
@@ -5,42 +5,41 @@ const { executeDeleteTables } = require('../../aws/delete-table');
 const { dynamoDB } = require('../../aws/dynamo');
 const { createReview } = require('../../aws/put-item');
 
-// beforeAll(async () => {
-//   await createTables();
-//   await createReview({
-//     reviewId: '1',
-//     likes: [{ S: 'user1' }],
-//     dislikes: [],
-//   });
-// });
+beforeAll(async () => {
+  await createTables();
+  await createReview({
+    reviewId: '1',
+    likes: [{ S: 'user1' }],
+    dislikes: [],
+  });
+});
 
-// afterAll(async () => {
-//   integrationServer.close();
-//   await executeDeleteTables();
-// });
+afterAll(async () => {
+  integrationServer.close();
+  await executeDeleteTables();
+});
 
-// const dislikeBody = {
-//   reviewId: '1',
-//   userId: 'user1',
-//   prevLike: 'like',
-//   currentLike: 'dislike',
-// };
+const dislikeBody = {
+  reviewId: '1',
+  userId: 'user1',
+  prevLike: 'like',
+  currentLike: 'dislike',
+};
 
-// test('should remove a like from reviews and add to dislikes', async () => {
-test('should remove a like from reviews and add to dislikes', () => {
-  // await request(integrationServer)
-  //   .post('/add-review')
-  //   .send(dislikeBody)
-  //   .expect(200);
-  // const params = {
-  //   TableName: 'reviews',
-  //   Key: {
-  //     reviewId: {
-  //       S: dislikeBody.reviewId,
-  //     },
-  //   },
-  // };
-  // const { Item: items } = await dynamoDB.getItem(params).promise();
-  // expect(items.dislikes.L.length).toBe(1);
-  // expect(items.likes.L.length).toBe(0);
+test('should remove a like from reviews and add to dislikes', async () => {
+  await request(integrationServer)
+    .post('/add-review')
+    .send(dislikeBody)
+    .expect(200);
+  const params = {
+    TableName: 'reviews',
+    Key: {
+      reviewId: {
+        S: dislikeBody.reviewId,
+      },
+    },
+  };
+  const { Item: items } = await dynamoDB.getItem(params).promise();
+  expect(items.dislikes.L.length).toBe(1);
+  expect(items.likes.L.length).toBe(0);
 });
